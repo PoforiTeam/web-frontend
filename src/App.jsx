ff0bd96ae5f27b42021b427012e2c39e03e6d17e
ingdol <dmswl3073@naver.com>
2024-06-13 03:20:49 +0900

    [feat] Implement routing and create Layout, Main, Resume pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./components/Layout";
+import Main from "./routes/Main";
+import Resume from "./routes/Resume";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { path: "", element: <Main /> },
+      { path: "resume", element: <Resume /> },
+    ],
+  },
+]);
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
---
+++ src/routes/Resume.jsx
@@ -0,0 +1,5 @@
+const Resume = () => {
+  return <>이력서 수정</>;
+};
+
+export default Resume;
