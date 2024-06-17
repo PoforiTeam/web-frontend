69fe190dd4ddf65ae6a0682d5aa5d5b2d419d88e
ingdol <dmswl3073@naver.com>
2024-06-18 01:54:19 +0900

    [style] Add CSS reset and configure Normalize.css

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import "./assets/styles/application.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
