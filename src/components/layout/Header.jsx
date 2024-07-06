ce7815a23540f7aaa38e72fb64a41d42bafa5498
ingdol <dmswl3073@naver.com>
2024-07-06 20:39:44 +0900

    [feat] Add submenu in header after login

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,14 +1,28 @@
+import { useEffect, useRef, useState } from "react";
 import { useLogout } from "../../api/hooks/useAuth";
 import { useAuthContext } from "../../context/AuthContext";
 
 const Header = ({ onLoginClick }) => {
   const { auth } = useAuthContext();
   const { mutate: logout } = useLogout();
+  const [isSubDropdown, setSubDropdown] = useState(false);
+  const dropdownRef = useRef(null);
+
+  const handleClickOutside = event => {
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+      setSubDropdown(false);
+    }
+  };
 
   const handleLogoutClick = () => {
     logout();
   };
-
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
   return (
     <header className="header">
       <a className="logo" href="/">
@@ -16,9 +30,21 @@ const Header = ({ onLoginClick }) => {
       </a>
 
       {auth ? (
-        <h1>
-          {auth.name} <button onClick={handleLogoutClick}>로그아웃</button>
-        </h1>
+        <>
+          <h1 onClick={() => setSubDropdown(!isSubDropdown)}>
+            <i className="xi-user-o" />
+            {auth.name}
+            {/* <button onClick={handleLogoutClick}>로그아웃</button> */}
+          </h1>
+          {isSubDropdown && (
+            <div className="sub-dropdown" ref={dropdownRef}>
+              <div onClick={() => navigate(`/resume/${list?.resume_id}`)}>
+                계정 관리
+              </div>
+              <div onClick={handleLogoutClick}>로그아웃</div>
+            </div>
+          )}
+        </>
       ) : (
         <button
           onClick={onLoginClick}
