31f155e19cfac30e92af3deb244fe3da4628c3ec
Jinyoung Choi <jinyoung5497@gmail.com>
2024-12-24 00:22:13 +0900

    [chore] prettierrc 파일 추가

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,21 +1,23 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    requireConfigFile: false,
+    babelOptions: {
+      presets: ['@babel/preset-react'],
+    },
+  },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
   rules: {
+    'react/react-in-jsx-scope': 'off',
     'react/jsx-no-target-blank': 'off',
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'no-unused-vars': ['warn', { varsIgnorePattern: 'React' }],
+    'react/prop-types': 'off',
   },
-}
+};
